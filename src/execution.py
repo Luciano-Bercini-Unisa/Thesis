b0284312c9394febd7f12175c10108b93bd81312
Luciano-Bercini-Unisa <l.bercini@studenti.unisa.it>
2026-04-24 18:16:46 +0200

    Improvements

--- src/execution.py
+++ src/execution.py
@@ -30,7 +30,7 @@ from .prompts import (
 # If GPU CUDA is available, then use bfloat16 (b stands for Brain in Google Brain), otherwise use float32.
 DTYPE = torch.bfloat16 if torch.cuda.is_available() else torch.float32
 VD_MAX_NEW_TOKENS = int(os.getenv("VD_MAX_NEW_TOKENS", "2048"))
-SA_MAX_NEW_TOKENS = int(os.getenv("SA_MAX_NEW_TOKENS", "128"))
+SA_MAX_NEW_TOKENS = int(os.getenv("SA_MAX_NEW_TOKENS", "256"))
 TEMPERATURE = float(os.getenv("TEMPERATURE", "0.7"))
 TOP_P = float(os.getenv("TOP_P", "1"))
 
